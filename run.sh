025b9ef059a40b49eb1689b00f5bd449a4a488c5
madjestic <madjestic13@gmail.com>
2020-06-23 14:10:09 +0200

    adding font rendering example

--- run.sh
+++ run.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 
-# Spinning Objects
-# a spinning box.
-# mvlink ./app/Main.hs /home/madjestic/Projects/Haskell/e1337/app/Main.hs.spinning_objects
-# cabal new-build
-# cabal new-run e1337 ./projects/spinning_objects
+# Broken!
+# # Spinning Objects
+# # a spinning box.
+# # mvlink ./app/Main.hs      /home/madjestic/Projects/Haskell/e1337/app/Main.hs.spinning_objects
+# # mvlink ./src/Rendering.hs /home/madjestic/Projects/Haskell/e1337/src/Rendering.hs.earth 
+# # cabal new-build
+# # cabal new-run e1337 ./projects/spinning_objects
 
 # Earth and ISS
 # planet Earth modelling and shading
-mvlink ./app/Main.hs /home/madjestic/Projects/Haskell/e1337/app/Main.hs.earth
+mvlink ./app/Main.hs      /home/madjestic/Projects/Haskell/e1337/app/Main.hs.earth
+mvlink ./src/Rendering.hs /home/madjestic/Projects/Haskell/e1337/src/Rendering.hs.earth 
 cabal new-build
 cabal new-run e1337 ./projects/earth_iss_v02
+
+# Font Rendering
+# learning to render ttf with SDL/OpenGL
+# mvlink ./app/Main.hs      /home/madjestic/Projects/Haskell/e1337/app/Main.hs.font
+# mvlink ./src/Rendering.hs /home/madjestic/Projects/Haskell/e1337/src/Rendering.hs.font
+# cabal new-build
+# cabal new-run e1337 ./projects/font
 
 # Earth and ISS S3 L1
 # planet Earth modelling and shading
@@ -37,14 +47,10 @@ cabal new-run e1337 ./projects/earth_iss_v02
 # cabal new-build
 # cabal new-run e1337 ./projects/debug_camera
 
-# Test
-# mvlink ./app/Main.hs /home/madjestic/Projects/Haskell/e1337/app/Main.hs.earth
-# cabal new-build
-# cabal new-run e1337 ./projects/test
-
-# Mandelbrot
-# playing with Mandelbrot
-# mvlink ./app/Main.hs /home/madjestic/Projects/Haskell/e1337/app/Main.hs.mandelbrot
-# cabal new-build
-# cabal new-run e1337 ./projects/mandelbrot
+# Broken!
+# # Mandelbrot
+# # playing with Mandelbrot
+# # mvlink ./app/Main.hs /home/madjestic/Projects/Haskell/e1337/app/Main.hs.mandelbrot
+# # cabal new-build
+# # cabal new-run e1337 ./projects/mandelbrot
 
